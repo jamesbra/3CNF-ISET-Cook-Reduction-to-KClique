380102151361f4e320552b77057820629f134861
Brandon James <brandonjames879@gmail.com>
2018-10-22 19:07:31 -0500

    3CNF and ISET reduction to Clique

--- src/solve3CNF.java
+++ src/solve3CNF.java
@@ -5,133 +5,126 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
-public class solve3CNF
-{
+public class solve3CNF {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		Scanner inputStream = null;
 		String fileName = args[0];
-		ArrayList<Graph> graphList = new ArrayList<Graph>();
-
-		try
-		{
+		int graphNumber = 0;
+		int edgeCount = 0;
+		solveClique sc = null;
+		try {
 			inputStream = new Scanner(new File(fileName));
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 
 		}
-		while (inputStream.hasNextLine())
-		{
-			// int numVar = inputStream.nextInt();
-			ArrayList<ArrayList<Integer>> matrix = new ArrayList<ArrayList<Integer>>();
-			matrix.add(new ArrayList<Integer>());
-			HashMap<Integer,int[]> clauseIndexGroups = new HashMap<Integer,int[]>();
-			// Graph tempGraph = new Graph();
+		while (inputStream.hasNextLine()) {
+			HashMap<Integer, int[]> clauseIndexGroups = new HashMap<Integer, int[]>();
 
-			// System.out.println("Variables = " + numVar);
-			// tempGraph.size = numVar * numVar;
-
-			// int[][] matrix = new int[numVar*numVar][numVar*numVar];
-			// int currentR = 0;
-			// int currentC = 0;
+			int currentC = 1;
 
 			// BUILD VARIABLE MAP - will fill out later
 			String temp = inputStream.nextLine();
 			String[] tokens = temp.split(" ");
 			int numVar = Integer.parseInt(tokens[0]);
-			System.out.println("Tokens length " + tokens.length);
-			// int tokenNum = 1;
-			matrix.add(new ArrayList<>());
-			matrix.get(0).add(0);
-			for (int i = 1; i < tokens.length; i++)
-			{
+			if (numVar == 0) {
+				break;
+			}
+			int sise = tokens.length;
+
+
+			int[][] m = new int[sise][sise];
+			int[] degree = new int[sise];
+
+			for (int i = 1; i < sise; i++) {
 
 				int token = Integer.parseInt(tokens[i]);
 				// Add to top column
-				matrix.get(0).add(token);
+				m[0][currentC] = token;
+				currentC++;
 				// Add to row
-				matrix.get(matrix.size() - 1).add(token);
-				// Add next row
-				matrix.add(new ArrayList<>());
+				m[i][0] = token;
 			}
-			//
-			System.out.println("Matrix size minus 2 " + (matrix.size()-2));
-			for (int i = 1; i<matrix.size()-1; i+=3){
-				int[] tempArray = {i,i+1,i+2};
+			//Build clause groups - should not connect nodes that are in the same group
+			for (int i = 1; i < m.length - 1; i += 3) {
+				int[] tempArray = { i, i + 1, i + 2 };
 				clauseIndexGroups.put(i, tempArray);
-				clauseIndexGroups.put(i+1, tempArray);
-				clauseIndexGroups.put(i+2, tempArray);
-			
+				clauseIndexGroups.put(i + 1, tempArray);
+				clauseIndexGroups.put(i + 2, tempArray);
+
 			}
-			
-			
-			// MAKING CONNECTIONS
+
+			// Building edge connections
 			// ROW
 			boolean existInGroup = false;
-			for (int i = 1; i < matrix.size()-1; i++)
-			{
+			for (int i = 1; i < m.length; i++) {
 				// COLUMN
-				for (int j = 1; j<matrix.size()-1; j++){
+				for (int j = 1; j < m.length; j++) {
 					int[] clauseGroup = clauseIndexGroups.get(i);
-					for (int k : clauseGroup){
-						if (j == k){
+					for (int k : clauseGroup) {
+						if (j == k) {
 							existInGroup = true;
-							matrix.get(i).add(0);
+							m[i][j] = 0;
 							break;
 						}
 					}
-					if (Math.abs(matrix.get(0).get(j)) == Math.abs(matrix.get(i).get(0)) && !existInGroup){
-						matrix.get(i).add(0);
+					if (Math.abs(m[0][j]) == Math.abs(m[i][0]) && !existInGroup) {
+						m[i][j] = 0;
 						continue;
 					}
-					if (!existInGroup){
-						matrix.get(i).add(1);
+					if (!existInGroup) {
+						m[i][j] = 1;
+						degree[i]++;
+						edgeCount++;
 						continue;
 					}
 					existInGroup = false;
 				}
 			}
 
-			for (ArrayList<Integer> i : matrix)
-			{
-				for (int j : i)
-				{
-					System.out.print(j + "\t");
+
+			int[][] newM = new int[m.length - 1][m.length - 1];
+			int copyR = 0;
+			int copyC = 0;
+
+			// Shrink array to eliminate non 1/0s
+			for (int i = 1; i < m.length; i++) {
+				for (int j = 1; j < m.length; j++) {
+					newM[copyR][copyC] = m[i][j];
+					copyC++;
 				}
-				System.out.println();
+				copyC = 0;
+				copyR++;
 			}
-			
-			
-			
-			
-			
-			// matrix.get(0).add(temp);
-			// matrix[currentR][0] = temp;
-			// matrix[0][currentC] = temp;
-			// currentR++;
-			// currentC++;
 
-			// for (int i = 0; i < matrix.length; i++)
-			// {
-			//
-			// for (int j = 0; j < matrix.length; j++)
-			// {
-			// // System.out.println(inputStream.nextInt());
-			// matrix[i][j] = inputStream.nextInt();
-			// System.out.print(matrix[i][j] + " ");
-			// }
-			// System.out.println();
-			// }
+			graphNumber++;
 
-			// tempGraph.setMatrix(matrix);
-			// if (tempGraph.size != 0)
-			// {
-			// graphList.add(tempGraph);
-			// }
-			// inputStream.nextLine();
+			// SOLVE CLIQUE
+			sc = new solveClique(numVar, newM, degree);
+			sc.search();
+
+
+			System.out.printf("\nG%d ( %d, %d ) {", graphNumber, sise, edgeCount / 2);
+			int vCount = 0;
+			for (int i = 0; i < sise; i++) {
+				if (sc.solution[i] == 1) {
+					vCount++;
+					System.out.print(" " + i);
+					if (i != sise - 1 && vCount != sc.maxSize) {
+						System.out.print(",");
+					}
+
+				}
+				if (i == sise - 1 || vCount == sc.maxSize) {
+					System.out.print(" } ");
+					break;
+				}
+			}
+			System.out.printf("( size=%d, %d ms)", sc.maxSize, System.currentTimeMillis() - sc.cpuTime);
+			System.out.println();
+			sc.display(newM);
+			
 		}
-	}
 
+	}
 }
--- src/solveISet.java
+++ src/solveISet.java
@@ -1,7 +1,120 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class solveISet
 {
-	public static void main(String[] argv){
-		
+	public static void main(String[] args)
+	{
+
+		Scanner inputStream = null;
+		String fileName = args[0];
+		ArrayList<Graph> graphList = new ArrayList<Graph>();
+		ArrayList<ArrayList<Integer>> matrix = new ArrayList<ArrayList<Integer>>();
+		File inFile = null;
+		BufferedReader br = null;
+		String currentRow; // row string read from file
+		int size = 0; // number of vertices in graph
+		int prevSize = 0; // Store previous size
+		int count = 0; // count for current row number of matrix
+		int edgeCount = 0;
+		int graphNumber = 0; // store current graph number from file
+		int[][] m = null; // 0/1 adjacency matrix
+		int[] values = null; // temporary row values
+		int[] degree = null; // number of edges that touch a vertex
+		solveClique sc = null;
+		try {
+			inFile = new File(args[0]);
+			// Create new FileReader to read input File
+			br = new BufferedReader(new FileReader(inFile));
+
+			System.out.printf("* Max Cliques in graphs in %s\n" + "   (|V|,|E|) Cliques (size, ms used) *", args[0]);
+			while ((currentRow = br.readLine()) != null) {
+				prevSize = size; // hold size
+				if (size == count) {
+					size = Integer.parseInt(currentRow);
+					// System.out.println(currentRow);
+					// System.out.println();
+					if (m != null) {
+						System.out.println();
+						graphNumber++;
+//						for (int[] i : m) {
+//							for (int j : i) {
+//								System.out.print(j + " ");
+//							}
+//							System.out.println();
+//						}
+						//solveClique(int size, int[][] m, int[] degree);
+						sc = new solveClique(prevSize, m, degree);
+						sc.search();
+						sc.display(m);
+
+						System.out.printf("\nG%d ( %d, %d ) {", graphNumber, prevSize, edgeCount / 2);
+						int vCount = 0;
+						for (int i = 0; i < prevSize; i++) {
+							if (sc.solution[i] == 1) {
+								vCount++;
+								System.out.print(" " + i);
+								if (i != prevSize - 1 && vCount != sc.maxSize) {
+									System.out.print(",");
+								}
+
+							}
+							if (i == prevSize - 1 || vCount == sc.maxSize) {
+								System.out.print(" } ");
+								break;
+							}
+						}
+						System.out.printf("( size=%d, %d ms)", sc.maxSize, System.currentTimeMillis() - sc.cpuTime);
+					}
+					m = new int[size][size];
+					degree = new int[size];
+					Arrays.fill(degree, 0);
+					edgeCount = 0;
+					count = -1;
+				} else {
+					values = Arrays.stream(currentRow.split(" ")).mapToInt(Integer::parseInt).toArray();
+					for (int i = 0; i < size; i++) {
+						if (i == count) {
+							m[count][i] = 0;
+						} else {
+							switch (values[i]) {
+							case 1:
+								//System.out.println("Case 1: "  + values[i]);
+								m[count][i] = 0;
+								break;
+							case 0:
+								//System.out.println("Case 0: " + values[i]);
+								m[count][i] = 1;
+								break;
+							}
+							if (m[count][i] == 1) {
+								degree[i]++;
+								edgeCount++;
+							}
+						}
+					}
+				}
+				count++;
+			}
+		} catch (IOException e) {
+			// Print Exception Stack Trace
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+			} catch (IOException ex) {
+				// Print Exception Stack Trace
+				ex.printStackTrace();
+			}
+			System.out.println("\n***");
+		}
+
 	}
 }
